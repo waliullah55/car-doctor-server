9e694dc5259b36e72e7a64809ae548b4e0dfadda
waliullah55 <waliullah9099@gmail.com>
2023-10-02 16:34:37 +0600

    node crud

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
-const products = require('./data/products.json');
-const service = require('./data/service.json');
-const team = require('./data/team.json');
-
+const products = require("./data/products.json");
+// const service = require('./data/service.json');
+const team = require("./data/team.json");
 
 // mdiileware
 app.use(express.json());
 app.use(cors());
 
-
 console.log(process.env.DB_USER);
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4njvdfp.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,47 +22,106 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db("carDoctor").collection("services");
+    const checkoutCollection = client.db("carDoctor").collection("checkouts");
+
+    // get data
+    app.get("/services", async (req, res) => {
+      const courser = serviceCollection.find();
+      const result = await courser.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        // projection: {description: 1, title:1, price:1},
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    // checkouts
+    app.get("/checkouts", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await checkoutCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/checkouts", async (req, res) => {
+      const checkout = req.body;
+      console.log(checkout);
+      const result = await checkoutCollection.insertOne(checkout);
+      res.send(result);
+    });
+
+    app.delete("/checkouts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await checkoutCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/checkouts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatingBooking = req.body;
+      const updateDoc = {
+        $set: {
+          status: updatingBooking.status
+        },
+      };
+      const result = await checkoutCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/products', (req, res) => {
-    res.send(products)
+app.get("/products", (req, res) => {
+  res.send(products);
 });
 
-app.get('/services', (req, res) => {
-    res.send(service)
+// app.get('/services', (req, res) => {
+//     res.send(service)
+// });
+
+// app.get('/services/:id', (req, res) => {
+//     const id = req.params.id;
+//     const speciphicId = service.find(n => n._id == id);
+//     res.send(speciphicId);
+// });
+
+app.get("/team", (req, res) => {
+  res.send(team);
 });
 
-app.get('/services/:id', (req, res) => {
-    const id = req.params.id;
-    const speciphicId = service.find(n => n._id == id);
-    res.send(speciphicId);
-});
-
-app.get('/team', (req, res) => {
-    res.send(team)
-});
-
-app.get('/', (req, res) => {
-    res.send("Car Doctor is waiting.......")
+app.get("/", (req, res) => {
+  res.send("Car Doctor is waiting.......");
 });
 
 app.listen(port, () => {
-    console.log(`Car Doctor is waiting on PORT: ${port}`);
-})
+  console.log(`Car Doctor is waiting on PORT: ${port}`);
+});
